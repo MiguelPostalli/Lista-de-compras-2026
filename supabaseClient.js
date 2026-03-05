4b78484354b91aa9790ac91bae3287bfa73758c9
MiguelPostalli <luizmiguelpostallielias@gmail.com>
2026-03-05 10:58:56 -0300

    M.P

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'SUPABASE_URL',
-  'SUPABASE_ANON_KEY'
+'https://tpmkdjgyhondslbkuwna.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwbWtkamd5aG9uZHNsYmt1d25hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE2MzMsImV4cCI6MjA4NzY4NzYzM30.iICcROY9dQXzDZV_y32MhYIC_jnGQBd3IicVNQ3VCv8'
 )
     
